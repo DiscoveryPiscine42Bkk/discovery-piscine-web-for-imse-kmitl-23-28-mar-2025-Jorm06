ec7c06c444733d03a685fdfa84fc9573f95adac3
Jorm06 <67160002@kmitl.ac.th>
2025-03-26 12:59:43 +0700

    modified:   cell03/ex04/ex00bis/backgroound.html 	deleted:    cell03/ex04/ex00bis/backgroound.js

--- cell03/ex04/ex00bis/backgroound.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $('#colorChanger').click(function() {
-        const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-        $('body').css('backgroundColor', randomColor);
-    });
-});
