c9f8c35d84d7c388f56cc53f22047c1e033535d6
Jorm06 <67160002@kmitl.ac.th>
2025-03-26 20:39:12 +0700

    modified:   cell03/ex04/ex03bis/todo.js

--- cell03/ex04/ex03bis/todo.js
+++ cell03/ex04/ex03bis/todo.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
     const $ft_list = $("#ft_list");
-    
+    const FirstKey = "FirstOpen";
    
     function createTodo() {
         const txt = prompt("Please Enter TODO LIST :");
         if (txt) {
             const name = new Date().getTime();
             
-           
             Cookies.set(name, txt);
             
             const $node = $("<div>").text(txt).click(function() {
@@ -20,20 +19,24 @@ $(document).ready(function() {
             $ft_list.prepend($node);
         }
     }
-    
-    
-    const allCookies = Cookies.get();
-    if (Object.keys(allCookies).length > 0) {
-        Object.keys(allCookies).forEach(function(key) {
-            const $node = $("<div>").text(allCookies[key]).click(function() {
-                if (confirm("Do you want to delete")) {
-                    $(this).remove();
-                    Cookies.remove(key);
-                }
-            });
-            $ft_list.prepend($node);
+      
+    if (!Cookies.get(FirstKey)) {
+       
+        Cookies.set(FirstKey, true);       
+    } else {
+        
+        Object.keys(Cookies.get()).forEach(function(key) {
+            if (key !== FirstKey) {
+                const $node = $("<div>").text(Cookies.get(key)).click(function() {
+                    if (confirm("Do you want to delete?")) {
+                        $(this).remove();
+                        Cookies.remove(key);
+                    }
+                });
+                $ft_list.prepend($node);
+            }
         });
     }
-    
+
     $("#newTodo").click(createTodo);
 });
