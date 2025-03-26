41b2652e1b1ff3d5aabbacd7ba8eb2010c299685
Jorm06 <67160002@kmitl.ac.th>
2025-03-26 16:22:30 +0700

    modified:   cell03/ex04/ex01bis/balloon.html 	deleted:    cell03/ex04/ex02bis/calc.css 	modified:   cell03/ex04/ex02bis/calc.html 	deleted:    cell03/ex04/ex02bis/calc.js 	modified:   cell03/ex04/ex03bis/index.html 	modified:   cell03/ex04/ex03bis/todo.js

--- cell03/ex04/ex03bis/todo.js
+++ cell03/ex04/ex03bis/todo.js
@@ -6,12 +6,14 @@ $(document).ready(function() {
         const txt = prompt("Please Enter TODO LIST :");
         if (txt) {
             const name = new Date().getTime();
-            document.cookie = `${name}=${txt}`;
+            
+           
+            Cookies.set(name, txt);
             
             const $node = $("<div>").text(txt).click(function() {
                 if (confirm("Do you want to delete")) {
                     $(this).remove();
-                    document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+                    Cookies.remove(name);
                 }
             });
             
@@ -19,20 +21,19 @@ $(document).ready(function() {
         }
     }
     
-    const cookies = document.cookie;
-    if (cookies.length > 0) {
-        const cookieArray = cookies.split(";");
-        cookieArray.forEach(function(element) {
-            const $node = $("<div>").text(element.split("=")[1]).click(function() {
+    
+    const allCookies = Cookies.get();
+    if (Object.keys(allCookies).length > 0) {
+        Object.keys(allCookies).forEach(function(key) {
+            const $node = $("<div>").text(allCookies[key]).click(function() {
                 if (confirm("Do you want to delete")) {
                     $(this).remove();
-                    document.cookie = element.split("=")[0] + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+                    Cookies.remove(key);
                 }
             });
             $ft_list.prepend($node);
         });
     }
     
-    
     $("#newTodo").click(createTodo);
-  });
+});
